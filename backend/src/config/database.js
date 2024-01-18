87d54518653414ff72e32347d67d21abce669c02
gslcloud <154714796+gslcloud@users.noreply.github.com>
2024-01-18 14:54:30 -0600

    init

---
+++ backend/src/config/database.js
@@ -0,0 +1,13 @@
+// database.js
+
+// Database configuration
+const databaseConfig = {
+  host: 'localhost',
+  port: '5432',
+  username: 'postgres',
+  password: 'password',
+  database: 'cash_saver_db',
+};
+
+module.exports = databaseConfig;
+```
